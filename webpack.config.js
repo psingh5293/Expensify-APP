72d9c59abc31e12ba80effc086c5f0f3357464ba
Prakash Kumar <prakashkumar@users-MacBook-Pro.local>
2018-12-27 01:27:53 +0530

    webpack config chnages

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,9 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
-
-  const isProduction = env ==='production';
+  const isProduction = env === 'production';
   const CSSExtract = new ExtractTextPlugin('styles.css');
-  console.log('env',env)
+
   return {
     entry: './src/app.js',
     output: {
@@ -20,30 +19,30 @@ module.exports = (env) => {
       }, {
         test: /\.s?css$/,
         use: CSSExtract.extract({
-          use:[
+          use: [
             {
-              loader:'css-loader',
-              options:{
-                sourceMap : true
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
               }
             },
             {
-              loader:'sass-loader',
-              options:{
-                sourceMap : true
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
               }
             }
           ]
         })
       }]
     },
-    plugins:[
+    plugins: [
       CSSExtract
     ],
-    devtool: isProduction ? 'source-map':'inline-source-map',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true
     }
   };
-}
+};
