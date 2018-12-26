68cf4f44459027abc437635023a03c7d6fa95b99
Prakash Kumar <prakashkumar@users-MacBook-Pro.local>
2018-12-27 01:21:49 +0530

    server.js update

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-const publicPath = path.join(__dirname,'..','public');
-const port = process.env.PORT || 3000
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.get('*',(req,res)=> {
-    res.sendFile(path.join(publicPath,'index.html'))
-})
-app.listen(port,()=>{
-    console.log('app is running')
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Server is up!');
+});
